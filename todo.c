989cce4fbc11bdd80ef9a6a90843a5d3461e35c6
Shivam Jain <90458496+Shivam-jainn@users.noreply.github.com>
2022-11-23 16:35:49 +0000

    Created todo.c
    Distributed tasks

---
+++ todo.c
@@ -0,0 +1,20 @@
+// linked list implementation
+
+#include <stdio.h>
+
+struct task
+{
+    char taskname;
+    int priority;
+    struct task* nexttask;
+};
+
+
+void inserttask();  // ankush
+
+void completetask(); // saanvi
+ 
+void showtask(); //shivam
+
+void prioritytask(); // ravi
+
