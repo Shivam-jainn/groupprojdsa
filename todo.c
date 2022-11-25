77569e318eb7a59ef1712727e8165cb2c41e82ea
Shivam Jain <shivam.clgstash@gmail.com>
2022-11-26 00:42:42 +0530

    show list task roughly done

--- todo.c
+++ todo.c
@@ -4,17 +4,28 @@
 
 struct task
 {
-    char taskname;
+    char taskname[100];
     int priority;
     struct task* nexttask;
 };
+
+typedef struct task task;
 
 
 void inserttask();  // ankush
 
 void completetask(); // saanvi
  
-void showtask(); //shivam
+void showtask(task* headptr){
+    task* temp = headptr  ;
+    while (temp->nexttask!=NULL)
+    {
+        
+        puts(temp->taskname);
+    }
+    
+
+} //shivam
 
 void prioritytask(); // ravi
 
